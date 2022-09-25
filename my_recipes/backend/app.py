9289cf55a0d0082496e870474afd2c808c209877
sjetalpuria <shivani.jetalpuria@gmail.com>
2022-09-24 19:07:31 -0700

    Backend completed

--- my_recipes/backend/app.py
+++ my_recipes/backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from bson.objectid import ObjectId
 from bson.json_util import dumps
 from json import loads
 
@@ -27,7 +28,7 @@ def my_profile():
 def login(username, password):
     result = users.find_one({"username": username})
     result = loads(dumps(result))
-    
+    # print(result)
     if result is None: # username not found in database
         response_body = {
             "message": "user not found"
@@ -84,6 +85,23 @@ def register():
 #       },
 #   ]
 # }
+@api.route("/view-recipe/<user_id>", methods=["GET"])
+def view_recipe(user_id):
+    result = recipes.find({"user_id": user_id})
+    result = loads(dumps(result))
+    # result = [{recipe_name: "asdada", "ing": ["As", "As"], "_id": dasda}, {}]
+    # print(result)
+    if result is None: # recipe does not exists
+        response_body = {
+            "message": "recipe not found"
+        }
+        return response_body, 500
+    else:
+        response_body = {
+            "message": "recipe found!",
+            "recipes": result
+        }
+        return response_body, 520
 ###########################################################
 # delete recipe - POST
 # input: recipe id (ex: "8910201ab22932bcf")
@@ -91,6 +109,18 @@ def register():
 # {
 #   "message": "successfully deleted"
 # }
+@api.route("/delete-recipe/", methods=["POST"])
+def delete_recipe():
+    data = request.get_json() # get data passed as json
+    recipe_id = data["recipe_id"]
+    recipes.delete_one({"_id": ObjectId(recipe_id)})
+    response_body = {
+        "message": "successfully deleted"
+    }
+    return response_body, 200
+
+    
+    
 ###########################################################
 # add recipe - POST
 # input: user_id and recipe json
@@ -101,6 +131,20 @@ def register():
 #   "instructions": ["Add some ice cubes in cup", "Add some water", "Enjoy"]
 # }
 # output: recipe id
+@api.route("/add-recipe/", methods=["POST"])
+def add_recipe():
+    data = request.get_json() # get data passed as json
+    user_id = data["user_id"]
+    recipe_name = data["recipe_name"]
+    ingredients = data["ingredients"]
+    instructions = data["instructions"]
+    results= recipes.insert_one( {"user_id":user_id, "recipe_name":recipe_name, 
+                                 "ingredients":ingredients, "instructions":instructions})
+    response_body = {
+        "recipe_id":str(results.inserted_id),
+        "message": "successfully added"
+    }
+    return response_body, 200
 
 ###########################################################
 # update recipe - POST
@@ -112,3 +156,19 @@ def register():
 #   "instructions": ["Add some ice cubes in cup", "Add some water", "Enjoy"]
 # }
 # output: successful message
+
+@api.route("/update-recipe/", methods=["POST"])
+def update():
+    data = request.get_json() # get data passed as json
+    recipe_id = data["recipe_id"]
+    recipe_name = data["recipe_name"]
+    ingredients = data["ingredients"]
+    instructions = data["instructions"]
+    new_result={"$set": {"_id": ObjectId(recipe_id), "recipe_name": recipe_name, "ingredients": ingredients,
+                          "instructions": instructions}}
+    recipes.update_one({"_id": ObjectId(recipe_id)},new_result)
+
+    response_body = {
+       "message": "successfully updated"
+    }
+    return response_body, 200
